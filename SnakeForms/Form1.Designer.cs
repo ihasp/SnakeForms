53036fd72cd059c670417e6d184fc8f8cb0d0ce1
Patryk <97395198+ihasp@users.noreply.github.com>
2023-09-09 17:13:41 +0200

    Icon

--- SnakeForms/Form1.Designer.cs
+++ SnakeForms/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Snake));
             this.lblScore = new System.Windows.Forms.Label();
             this.lblFood = new System.Windows.Forms.Label();
             this.SuspendLayout();
@@ -58,6 +59,7 @@
             this.Controls.Add(this.lblScore);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(5);
             this.Name = "Snake";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
